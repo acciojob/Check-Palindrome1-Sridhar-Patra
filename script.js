2bb0cb002e6c0c6b734916c5974c48f1d34b8568
Sridhar Patra <73095300+Sridhar-Patra@users.noreply.github.com>
2023-05-19 23:00:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+s=str.toLowerCase();
+	let ispalindrom=true;
+	let i=0 , j=s.length-1;
+	while(i<=j && ispalindrom){
+		if(s[i]!==s[j]){
+			ispalindrom=false;
+			break;
+		}
+		i++;
+		j--;
+	}  
+	console.log(ispalindrom ? "true" : "false");
+	
 }
 module.exports = palindrome
